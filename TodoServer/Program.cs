2a40591846fb8db81aaa57f862a81d032a490d18
Jan Chester <janchesterperen@gmail.com>
2025-02-25 16:24:06 +0800

    Add project files.

---
+++ TodoServer/Program.cs
@@ -0,0 +1,41 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using TodoServer.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient("UnsecureAPI", client =>
+{
+    client.BaseAddress = new Uri("https://10.0.0.12:7081/");
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+});
+
+builder.Services.AddSweetAlert2();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
